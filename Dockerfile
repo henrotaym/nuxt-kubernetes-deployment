46fcd0b694a59e9bcbd5b678f57906d42cc12470
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2023-04-26 12:30:19 +0200

    :alembic: Basic example to test from .

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,31 @@
 FROM node:16-alpine as deps
 
+USER node
+
 WORKDIR /app
 
-COPY package.json yarn.lock ./
+COPY --chown=node:node package.json yarn.lock ./
 
-RUN yarn install
+RUN yarn install --frozen-lockfile --ignore-scripts
 
 # BUILDER
 FROM deps as builder
 
+USER node
+
 WORKDIR /app
 
-COPY --from=deps /app/node_modules ./node_modules
+COPY --from=deps --chown=node:node /app/node_modules ./node_modules
 
-COPY . .
+COPY --chown=node:node . .
 
 # RUNNER
 FROM builder as runner
 
+USER node
+
 WORKDIR /app
 
-COPY --from=builder /app ./
+COPY --chown=node:node --from=builder /app ./
 
-CMD yarn install && yarn dev
+CMD yarn dev
---
+++ bin/devops.js
@@ -0,0 +1,94 @@
+#!/usr/bin/env node
+
+"use strict";
+
+import {
+  mkdirSync,
+  readFileSync,
+  readdirSync,
+  writeFileSync,
+  lstatSync,
+  rmSync,
+} from "fs";
+import { resolve } from "path";
+import { argv } from "process";
+
+const trying = (expected) => {
+  try {
+    return [null, expected()];
+  } catch (error) {
+    return [error, null];
+  }
+};
+
+const parametersMapping = {
+  "--key": "{MODULE_NAME}",
+  "--variable": "{MODULE_VARIABLE}",
+  "--env": "{MODULE_ENVIRONMENT}",
+};
+
+const commandParameterNames = Object.keys(parametersMapping);
+
+const [, , ...commandArgs] = argv;
+
+const valuesMapping = commandArgs.reduce((valuesMapping, commandArg) => {
+  const [key, value] = commandArg.split("=");
+  return {
+    ...valuesMapping,
+    [key]: value,
+  };
+}, {});
+
+const environment = valuesMapping["--env"] || "production";
+
+const currentPath = resolve(process.cwd());
+const stubsPath = resolve(currentPath, "stubs");
+const devopsPath = resolve(currentPath, "devops");
+const environmentDevopsPath = resolve(devopsPath, environment);
+
+const resetDevopsFolder = () => {
+  const [, isExisting] = trying(() => lstatSync(devopsPath).isDirectory());
+  if (!isExisting) return mkdirSync(devopsPath);
+  trying(() => rmSync(environmentDevopsPath, { recursive: true, force: true }));
+};
+
+const generateEnvironmentDevops = (
+  currentStubPath = stubsPath,
+  currentDevopsPath = environmentDevopsPath
+) => {
+  const elements = readdirSync(currentStubPath);
+  if (!elements.length) return;
+  mkdirSync(currentDevopsPath);
+
+  elements.forEach((element) => {
+    const elementStubPath = resolve(currentStubPath, element);
+    console.log(elementStubPath);
+    const elementDevopsPath = resolve(
+      currentDevopsPath,
+      element.replace(".stub", "")
+    );
+    const isDirectory = lstatSync(elementStubPath).isDirectory();
+    if (isDirectory)
+      return generateEnvironmentDevops(elementStubPath, elementDevopsPath);
+
+    const content = readFileSync(elementStubPath, {
+      encoding: "utf8",
+      flag: "r",
+    });
+
+    const replacedContent = commandParameterNames.reduce(
+      (replacedContent, parameter) => {
+        const { [parameter]: value } = valuesMapping;
+        if (!value) return replacedContent;
+        const { [parameter]: key } = parametersMapping;
+        return replacedContent.replace(new RegExp(key, "g"), value);
+      },
+      content
+    );
+
+    writeFileSync(elementDevopsPath, replacedContent);
+  });
+};
+
+resetDevopsFolder();
+generateEnvironmentDevops();
